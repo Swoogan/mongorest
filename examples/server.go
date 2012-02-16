3c55eb68502e80ecd36a2c88722be13ad6aec0b8
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-16 15:34:30 -0600

    Improved the example application

--- examples/server.go
+++ examples/server.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"http"
 	"log"
+	"http"
+	"flag"
 	"launchpad.net/mgo"
 	"bitbucket.org/Swoogan/mongorest"
 )
 
 func main() {
-	log.Printf("Connecting to mongodb")
+	var mongo *string = flag.String("m", "localhost", "Mongodb address")
+	var dbname *string = flag.String("d", "test", "Mongodb database name")
+	var address *string = flag.String("a", ":8080", "Address to listen on")
+	flag.Parse()
 
-	session, err := mgo.Mongo("localhost")
+	log.Printf("Connecting to mongodb at %v", *mongo)
+
+	session, err := mgo.Mongo(*mongo)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	defer session.Close()
 
-	db := session.DB("test")
+	log.Printf("Opening database %v", *dbname)
+	db := session.DB(*dbname)
 
 	mongorest.New(db, "customers")
 	mongorest.New(db, "employees")
 
-	log.Printf("About to listen on 8080")
-	err = http.ListenAndServe(":8080", nil)
+	log.Printf("About to listen on %v", *address)
+	err = http.ListenAndServe(*address, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
