d37a5b99abc2a99152615fb63be2df1491e84d92
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-17 08:19:39 -0600

    Added a directory for the server

---
+++ mongorestd/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"http"
+	"flag"
+	"launchpad.net/mgo"
+	//	"bitbucket.org/Swoogan/mongorest"
+	"mongorest"
+)
+
+func main() {
+	var mongo *string = flag.String("m", "localhost", "Mongodb address")
+	var dbname *string = flag.String("d", "test", "Mongodb database name")
+	var address *string = flag.String("a", ":8080", "Address to listen on")
+	flag.Parse()
+
+	log.Printf("Connecting to mongodb at %v", *mongo)
+
+	session, err := mgo.Mongo(*mongo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	log.Printf("Opening database %v", *dbname)
+	db := session.DB(*dbname)
+
+	mongorest.New(db, "customers")
+	mongorest.New(db, "employees")
+
+	log.Printf("About to listen on %v", *address)
+	err = http.ListenAndServe(*address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
